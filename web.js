11f83433b15b80f4d7ea5812d0a1a40991dcb95a
Todd Treece <toddtreece@gmail.com>
2014-09-16 11:31:40 -0400

    adds web interface

---
+++ web.js
@@ -0,0 +1,43 @@
+var util = require('util'),
+    express = require('express'),
+    app = express(),
+    Lumix = require('./index'),
+    images = Lumix.ImageStream(),
+    lumix = Lumix();
+
+lumix.once('ready', function() {
+
+  lumix.request.mode('setsettings').type('liveviewsize').value('vga').queue();
+  lumix.request.mode('camcmd').value('recmode').queue();
+  lumix.request.mode('startstream').value(images.port).queue();
+
+});
+
+app.get('/', function(req, res) {
+  res.send('<img src="lumix.mjpeg">');
+});
+
+app.get('/lumix.mjpeg', function(req, res) {
+
+  res.writeHead(200, {
+    'Content-Type': 'multipart/x-mixed-replace; boundary=lumix',
+    'Cache-Control': 'no-cache',
+    'Connection': 'close',
+    'Pragma': 'no-cache'
+  });
+
+  images.on('data', function(image) {
+
+    res.write('--lumix\r\n');
+    res.write('Content-Type: image/jpeg\r\n');
+    res.write('Content-Length: ' + image.length + '\r\n');
+    res.write('\r\n');
+    res.write(image, 'binary');
+    res.write('\r\n');
+
+  });
+
+});
+
+app.listen(8080);
+
